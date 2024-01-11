7d039f2e174e9e809192ed60e68ca9b7cdefbc28
beloow <quevaqueva11@gmail.com>
2024-01-11 10:35:10 +0100

    + responsive navbar

--- js/script.js
+++ js/script.js
@@ -2,23 +2,18 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 // scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-
-
 window.onscroll = () => {
 
-
-    // sticky header
-    let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-
-    // remove toggle icon and navbar when click navbar links (scroll)
+    // active navbar
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 100;
@@ -32,6 +27,15 @@ window.onscroll = () => {
             });
         }
     });
+
+    // sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 
 
     // animation footer on scroll
