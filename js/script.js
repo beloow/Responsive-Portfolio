64cbbf9b80d9b25c95fc2ab0cafa3bb2397e7aa2
beloow <quevaqueva11@gmail.com>
2024-01-11 10:01:50 +0100

    + active links

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
+
+
 window.onscroll = () => {
 
 
@@ -17,6 +19,19 @@ window.onscroll = () => {
 
 
     // remove toggle icon and navbar when click navbar links (scroll)
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
 
 
     // animation footer on scroll
