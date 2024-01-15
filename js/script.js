b48cc6f64c22f83db1fb651ebb98f4e6b7d61e87
beloow <quevaqueva11@gmail.com>
2024-01-15 12:13:01 +0100

    + responsive 462px

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 // toggle icon navbar
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
-
-// scroll sections
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
 
